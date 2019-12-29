66dbb44fe83bb930206a6b70e942ab0ac31c0777
Destiny Ajakaiye <destinyajakaiye@gmail.com>
2019-12-29 18:48:00 +0100

    Add hgome features

---
+++ build/precache-manifest.6f41178b6ca33d8539b15d4a230d828f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62123497c14979a2088eeba03341e971",
+    "url": "/index.html"
+  },
+  {
+    "revision": "141afbc659033f5ac48f",
+    "url": "/static/css/main.4bd4786c.chunk.css"
+  },
+  {
+    "revision": "c3d40aa1c8ffa689eb79",
+    "url": "/static/js/2.7c79bc48.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7c79bc48.chunk.js.LICENSE"
+  },
+  {
+    "revision": "141afbc659033f5ac48f",
+    "url": "/static/js/main.98cc2e17.chunk.js"
+  },
+  {
+    "revision": "7ce3af935cb8fe30c945",
+    "url": "/static/js/runtime-main.b39aeb2f.js"
+  },
+  {
+    "revision": "acf9f60550ddb034b0998879cd2469c7",
+    "url": "/static/media/destiny_ajakaiye.acf9f605.jpeg"
+  },
+  {
+    "revision": "aa0b79ae29327e61387850397e7e867a",
+    "url": "/static/media/github.aa0b79ae.png"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,124 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import logo from '../src/assets/images/destiny_ajakaiye.jpeg';
+import Twitter from '../src/assets/images/facebook.png';
+import Github from '../src/assets/images/github.png';
+import LinkedIn from '../src/assets/images/linkedin.png';
+import Medium from '../src/assets/images/linkedin.png';
 import './App.css';
+import '../src/assets/css/cssanimation.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Home extends Component {
+  
+  state = {
+    stepOne: true,
+    stepTwo: false,
+    stepThree: false,
+    stepFour: false,
+    button: false,
+    runner: 1
+  };
+
+  _switchOne = () => {
+    this.setState({
+      stepOne: false,
+      stepTwo: true,
+      stepFour: false,
+      runner: 2
+    });
+  };
+
+  _switchTwo = () => {
+      this.setState({
+        stepOne: false,
+        stepTwo: false,
+        stepThree: true,
+        stepFour: false,
+        runner: 3
+      });
+  }
+
+  _switchThree = () => {
+    
+      this.setState({
+        stepOne: false,
+        stepTwo: false,
+        stepThree: false,
+        stepFour: true,
+        runner: 1,
+      });
+  }
+
+  _runner = () => {
+    if(this.state.runner === 1) {
+      this._switchOne();
+    }
+    if(this.state.runner === 2) {
+      this._switchTwo();
+    }
+    if(this.state.runner === 3) {
+      this._switchThree();
+    }
+  }
+
+  render() {
+    let content = 'About Me';
+    const { stepOne, stepTwo, stepThree, stepFour, runner } = this.state;
+
+    if(runner === 2) {
+      content = 'What I Do';
+    }
+    if(runner === 3) {
+      content = 'Contact Me';
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div>
+            {stepOne && (
+              <>
+              <p className={`cssanimation typing sequence`}>Hi, welcome to my home.</p><br/>
+              </>
+            )}
+            {stepTwo && (
+              <>
+              <p className="cssanimation typing sequence">My name is Destiny Ajakaiye.</p><br/>
+              </>
+            )}
+            {stepThree && (
+              <>
+              <p className="cssanimation typing sequence">I'm a Software Developer.</p><br/>
+              </>
+            )}
+
+            {stepFour && (
+              <>
+                <p>
+                  <a href="https://www.twitter.com/@DestinyAjax" target="blank" title="Twitter">
+                    <img src={Twitter} className="App-social-icon" alt="..." />
+                  </a>
+                  <a href="https://www.linkedin.com/in/destinyajax/" target="blank" title="Linkedin">
+                    <img src={LinkedIn} className="App-social-icon" alt="..." />
+                  </a>
+                  <a href="https://www.github/destinyajax" target="blank" title="Github">
+                    <img src={Github} className="App-social-icon" alt="..." />
+                  </a>
+                  <a href="https://medium.com/@destinyajax" target="blank" title="Medium">
+                    <img src={Medium} className="App-social-icon" alt="..." />
+                  </a>
+                </p><br/>
+              </>
+            )}
+            
+            <button onClick={this._runner} className="cssanimation hu__hu__ sequence" type="button">
+              {content}
+            </button>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Home;
